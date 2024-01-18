1068d0ccd190c3c4590f8607d5f7b58f605d03b1
minh711 <duyminhtruong711@gmail.com>
2024-01-18 22:00:06 +0700

    Character movement animation version 1.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     public float speed;
 
+    public Animator animator;
+
     // get input from player
     // apply movement to sprite
 
@@ -16,6 +18,26 @@ public class Movement : MonoBehaviour
 
         Vector3 direction = new Vector2(horizontal, vertical);
 
+        AnimateMovement(direction);
+
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
